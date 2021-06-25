8e0fa796e9412cdf6cb804bd6f447a229bbdc5e1
nicomorenof <85754541+nicomorenof@users.noreply.github.com>
2021-06-25 18:10:44 +0200

    1
    1

---
+++ defines.php
@@ -0,0 +1,4 @@
+<?php
+	define('INSTAGRAM_APP_ID','849451735777790');
+	define('INSTAGRAM_APP_SECRET','78bac807bedbb479235df8f032105cb9');
+	define('INSTAGRAM_APP_REDIRECT_URL','https://github.com/nicomorenof/insta/blob/main/apiblog');
